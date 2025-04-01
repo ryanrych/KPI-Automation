16af2f7198dbf7b788a7c94c7b60f6cbe4797701
ryanrych <ryanjrychlak@gmail.com>
2025-04-01 11:29:51 -0500

    added external dppm

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import mysql.connector
 
 connection = mysql.connector.connect(
@@ -45,10 +47,56 @@ def get_internal_dppm(start_date, end_date):
     return internal_dppm
 
 
+def get_external_dppm(start_date, end_date):
+    """
+    retrieves parts shipped to and returned from all clients between the given dates (inclusive)
 
+    :param start_date: start date in YYYY-MM-DD format
+    :param end_date: end date in YYYY-MM-DD format
+    :return: dictionary pairing all client codes to the parts they received and returned
+    """
 
+    external_dppm = {}
+    query = ("SELECT SD_ORDER_N, SD_TOT_QTY "
+             "FROM ship_det "
+             "WHERE SD_SHIP_DA >= %s "
+             "AND SD_SHIP_DA <= %s;")
+    cursor.execute(query, (start_date, end_date))
+    response = cursor.fetchall()
+    for row in response:
 
-print(get_internal_dppm("2025-02-01", "2025-02-28"))
+        if not row[0] or not row[1]:
+            continue
+
+        query = "SELECT OR_CUST_CO FROM `order` WHERE OR_ORDER_N = %s;"
+        cursor.execute(query, (row[0],))
+        order_row = cursor.fetchone()
+        if not order_row:
+            continue
+
+        client_code = order_row[0]
+
+        if client_code not in external_dppm:
+            if row[1] > 0:
+                external_dppm[client_code] = {
+                    "shipped": float(row[1]),
+                    "returned": 0
+                }
+            else:
+                external_dppm[client_code] = {
+                    "shipped": 0,
+                    "returned": -1 * float(row[1])
+                }
+        else:
+            if row[1] > 0:
+                external_dppm[client_code]["shipped"] += float(row[1])
+            else:
+                external_dppm[client_code]["returned"] += -1 * float(row[1])
+
+    return external_dppm
+
+external = get_external_dppm("2025-02-01", "2025-02-28")
+print(json.dumps(external, indent=2))
 
 cursor.close()
 connection.close()
